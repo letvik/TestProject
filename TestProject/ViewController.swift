aede3e559a5af169998d3276928cd0c60c9fa7e7
Roman Lytvynenko <roman.lytvynenko@yahoo.com>
2019-11-24 15:09:35 +0100

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Roman Lytvynenko on 24.11.2019.
+//  Copyright © 2019 Roman Lytvynenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
